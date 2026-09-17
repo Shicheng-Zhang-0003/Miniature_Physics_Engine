04934fa79956be48f562ffae0de6679b20b909ce
Shicheng_Zhang-0003 <shicheng_zhang@outlook.com>
2026-09-16 21:28:11 -0600

    Fix mouse lock in GTK4 path

--- v15S/src/ui_input/input_control.c
+++ v15S/src/ui_input/input_control.c
@@ -111,8 +111,10 @@ gboolean on_button_press(GtkGestureClick *gest, int n_press, double x, double y,
     if(btn==1 && !st->is_mouse_locked) {
         st->mouse_delta_x=0; st->mouse_delta_y=0;
         st->is_mouse_locked=true;
+        mouse_lock_enable(gtk_event_controller_get_widget(GTK_EVENT_CONTROLLER(gest)));
     } else if(!st->is_mouse_locked) {
         st->is_mouse_locked=true;
+        mouse_lock_enable(gtk_event_controller_get_widget(GTK_EVENT_CONTROLLER(gest)));
     }
     return FALSE;
 }
@@ -135,7 +137,10 @@ gboolean on_focus_out(GtkEventControllerFocus *ctrl, gpointer user_data_stored) 
     st->stability_test_pressed=false; st->sleep_wake_test_pressed=false; st->editor_torture_pressed=false; st->spawn_stress_pressed=false; st->validation_report_pressed=false;
     st->long_run_validation_pressed=false; st->config_torture_pressed=false; st->debug_terminal_pressed=false; st->enter_spawn_held=false;
     st->mouse_delta_x=0; st->mouse_delta_y=0; st->right_mouse_button_clicked=false; st->middle_mouse_button_clicked=false; st->suppress_mouse_delta=false;
-    if(st->is_mouse_locked) st->is_mouse_locked=false;
+    if(st->is_mouse_locked) {
+        mouse_lock_disable(gtk_event_controller_get_widget(GTK_EVENT_CONTROLLER(ctrl)));
+        st->is_mouse_locked=false;
+    }
     return FALSE;
 }
 
